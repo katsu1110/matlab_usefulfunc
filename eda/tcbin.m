bb96304c3301be8efb812df95bbf108399377b93
katsu1110 <code1110g-show@hotmail.co.jp>
2018-01-24 19:57:15 +0100

    tcbin can treat matrix too

--- eda/tcbin.m
+++ eda/tcbin.m
@@ -1,17 +1,17 @@
 function [binned] = tcbin(v, binsize)
 %%
-% bin the vector v based on the specified binsize.
+% bin the vector v (can be matrix) based on the specified binsize.
 % note that this binning simply splits 'v' into 'binsize' elements
 % from the beginning to the end (different from a 'hist' like method).
 %
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-lenv = length(v);
+lenv = size(v,2);
 r = mod(lenv, binsize);
-binned = floor(lenv/binsize)*ones(1, binsize);
+binned = floor(lenv/binsize)*ones(size(v,1), binsize);
 i = 1;
 while r > 0
-    binned(i) = binned(i) + 1;
+    binned(1,i) = binned(1,i) + 1;
     r = r - 1;
     if i <= binsize
         i = i + 1;
@@ -21,8 +21,8 @@ while r > 0
 end
 begin = 1;
 for b = 1:binsize
-    step = binned(b);
-    binned(b) = nanmean(v(begin:begin+step-1));
+    step = binned(1,b);
+    binned(:,b) = nanmean(v(:,begin:begin+step-1),2);
     begin = begin + step;
 end
 
