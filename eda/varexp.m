90ac6860a4c5e72b584d19a995d98e91ae18dd78
katsu1110 <katsuhisa.kawaguchi@uni-tuebingen.de>
2018-05-31 19:17:51 +0200

    rsquare as variance explained

--- eda/varexp.m
+++ eda/varexp.m
@@ -1,4 +1,7 @@
 function variance_explained = varexp(yorig, ypred)
-% compute variance explained
-me = mean(yorig);
-variance_explained = sum(sqrt((ypred - me).^2))/sum(sqrt((yorig - me).^2));
+% % compute variance explained
+% me = mean(yorig);
+% variance_explained = sum(sqrt((ypred - me).^2))/sum(sqrt((yorig - me).^2));
+
+rr = corrcoef(yorig, ypred);
+variance_explained = rr(1,2)^2;
