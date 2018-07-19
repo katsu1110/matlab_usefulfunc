004231823ac736fc59b0785aaa811d555fd3af4c
katsu1110 <katsuhisa.kawaguchi@uni-tuebingen.de>
2018-07-19 12:30:10 +0200

    confusion matrix added

--- eda/confusion_matrix.m
+++ eda/confusion_matrix.m
@@ -1,11 +1,26 @@
-function cm = confusion_matrix(vecs, corrtype)
+function cm = confusion_matrix(vecs, corrtype, mapcolor)
 % generate confusion matrix (correlation coefficient between pairs of the
 % given vectors)
 % INPUT:
 % vecs ... cell arrray each containing vector (vectors have to be the same
-% length)
+% length and vertical)
 % corrtype ... 'Pearson' or 'Spearman'
+% mapcolor ... color scheme of confusion matrix ('parula' in default)
 %
 
+if nargin < 2
+    corrtype = 'Pearson';
+end
+if nargin < 3
+    mapcolor = 'parula';
+end
 lenv = length(vecs);
 cm = nan(lenv, lenv);
+for r = 1:lenv
+    for c = 1:lenv
+        cm(r,c) = corr(vecs{r}, vecs{c}, 'type', corrtype);
+    end
+end
+imagesc(1:lenv, 1:lenv, cm)
+colormap(mapcolor)
+set(gca, 'box', 'off'); set(gca, 'TickDir', 'out')
