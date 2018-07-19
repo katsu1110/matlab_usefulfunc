fd3bc842f5939653eb21122b27aaf212af050fed
katsu1110 <katsuhisa.kawaguchi@uni-tuebingen.de>
2018-07-19 12:33:33 +0200

    confusion matrix with p added

--- eda/confusion_matrix.m
+++ eda/confusion_matrix.m
@@ -1,4 +1,4 @@
-function cm = confusion_matrix(vecs, corrtype, mapcolor)
+function [cm, pm] = confusion_matrix(vecs, corrtype, mapcolor)
 % generate confusion matrix (correlation coefficient between pairs of the
 % given vectors)
 % INPUT:
@@ -6,6 +6,10 @@ function cm = confusion_matrix(vecs, corrtype, mapcolor)
 % length and vertical)
 % corrtype ... 'Pearson' or 'Spearman'
 % mapcolor ... color scheme of confusion matrix ('parula' in default)
+%
+% OUTPUT:
+% cm ... confusion matrix
+% pm ... matrix for corresponding p-values
 %
 
 if nargin < 2
@@ -16,9 +20,10 @@ if nargin < 3
 end
 lenv = length(vecs);
 cm = nan(lenv, lenv);
+pm = nan(lenv, lenv);
 for r = 1:lenv
     for c = 1:lenv
-        cm(r,c) = corr(vecs{r}, vecs{c}, 'type', corrtype);
+        [cm(r,c), pm(r,c)] = corr(vecs{r}, vecs{c}, 'type', corrtype);
     end
 end
 imagesc(1:lenv, 1:lenv, cm)
