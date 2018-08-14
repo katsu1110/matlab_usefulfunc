3f8ecb61fefd78c70a20c5e9bb6815eca7a5c4ac
katsu1110 <katsuhisa.kawaguchi@uni-tuebingen.de>
2018-08-14 18:02:57 +0200

    check decorrelation in ps arti

--- eye/eyepos_vs_ps.m
+++ eye/eyepos_vs_ps.m
@@ -12,16 +12,7 @@ function data = eyepos_vs_ps(x, y, p)
 
 %%
 % correlation
-[r, pval] = corrcoef(x, p);
-data.Pearson.r(1) = r(1,2);
-data.Pearson.p(1) = pval(1,2);
-[r, pval] = corrcoef(y, p);
-data.Pearson.r(2) = r(1,2);
-data.Pearson.p(2) = pval(1,2);
-[data.Spearman.r(1), data.Spearman.p(1)]...
-    = corr(x', p', 'type', 'Spearman');
-[data.Spearman.r(2), data.Spearman.p(2)]...
-    = corr(y', p', 'type', 'Spearman');
+data.original = compute_correlation(x, y, p);
 
 %%
 % 2nd polynomial regression
@@ -29,3 +20,19 @@ data.polyreg_xparas = polyfit(p, x, 2);
 data.correct_x = x - polyval(data.polyreg_xparas, p);
 data.polyreg_yparas = polyfit(p, y, 2);
 data.correct_y = y - polyval(data.polyreg_yparas, p);
+
+%% 
+% check no correlation between corrected eye pos and ps
+data.corrected = compute_correlation(data.correct_x, data.correct_y, p);
+
+function out = compute_correlation(x, y, p)
+[r, pval] = corrcoef(x, p);
+out.Pearson.r(1) = r(1,2);
+out.Pearson.p(1) = pval(1,2);
+[r, pval] = corrcoef(y, p);
+out.Pearson.r(2) = r(1,2);
+out.Pearson.p(2) = pval(1,2);
+[out.Spearman.r(1), out.Spearman.p(1)]...
+    = corr(x', p', 'type', 'Spearman');
+[out.Spearman.r(2), out.Spearman.p(2)]...
+    = corr(y', p', 'type', 'Spearman');
